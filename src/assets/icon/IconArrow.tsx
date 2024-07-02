30d2d04d21c8f60667e90d108aaabf52defc3ebe
Cabreira97 <thiagovieiramachado97@gmail.com>
2024-07-02 19:03:29 -0300

    ✨Add IconArrow component

---
+++ src/assets/icon/IconArrow.tsx
@@ -0,0 +1,22 @@
+interface IconArrowProps {
+  className?: string;
+}
+
+export const IconArrow: React.FC<IconArrowProps> = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+      ></path>
+    </svg>
+  )
+}
